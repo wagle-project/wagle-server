dafa67a4024f84e6d8fde6141a6f3bfc7cecf2cb
Seohyun Kim <canna7610@gmail.com>
2026-02-15 22:45:06 +0900

    fix: 방문자 레파지토리의 key가 Long에서 String으로 정정

--- src/main/java/com/waglewagle/server/domain/visitor/repository/VisitorRepository.java
+++ src/main/java/com/waglewagle/server/domain/visitor/repository/VisitorRepository.java
@@ -3,5 +3,5 @@ package com.waglewagle.server.domain.visitor.repository;
 import com.waglewagle.server.domain.visitor.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VisitorRepository extends JpaRepository<Visitor, Long> {
+public interface VisitorRepository extends JpaRepository<Visitor, String> {
 }
