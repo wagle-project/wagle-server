2e516cf3fca2c6958d481cc80d30293b358e5425
Uzrt <wakhj47@naver.com>
2026-04-28 19:53:54 +0900

    timetable에 name 추가

--- src/main/java/com/waglewagle/server/domain/timeTable/entity/TimeTable.java
+++ src/main/java/com/waglewagle/server/domain/timeTable/entity/TimeTable.java
@@ -16,6 +16,9 @@ public class TimeTable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 100)
+    private String name;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "festival_id", nullable = false)
     private Festival festival;
