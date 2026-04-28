215fe24a3ff636aeed4ac69b13366d81d3781389
Uzrt <wakhj47@naver.com>
2026-04-28 14:19:23 +0900

    Service.java 파일 생성

---
+++ src/main/java/com/waglewagle/server/domain/festival/service/FestivalService.java
@@ -0,0 +1,7 @@
+package com.waglewagle.server.domain.festival.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FestivalService {
+}
---
+++ src/main/java/com/waglewagle/server/domain/timeTable/service/TimeTableService.java
@@ -0,0 +1,7 @@
+package com.waglewagle.server.domain.timeTable.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TimeTableService {
+}
