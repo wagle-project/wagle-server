6214401ea32e401277fa45d1433c9eab8f38cfbb
Seohyun Kim <canna7610@gmail.com>
2026-04-29 00:37:15 +0900

    fix: createAt이 null로 표시되는 문제 해결

--- src/main/java/com/waglewagle/server/domain/visitor/dto/VisitorDTO.java
+++ src/main/java/com/waglewagle/server/domain/visitor/dto/VisitorDTO.java
@@ -17,19 +17,19 @@ public class VisitorDTO {
 
     @Schema(description = "방문자 등록 완료 응답")
     public record VisitorResponse(
-            @Schema(description = "서버 생성 UUID (ID)", example = "550e8400-e29b-41d4-a716-446655440000")
+            @Schema(description = "서버 생성 UUID (ID)", example = "550e840...")
             String uuid,
 
             @Schema(description = "최초 등록 시간", example = "2026-02-18T12:28:14.15784")
             LocalDateTime createdAt,
 
-            @Schema(description = "엑세스 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjMsImlhdCI6MTc0NzU2MTcwOCwiZXhwIjoxNzQ3NTY1MzA4fQ.Pwm5AKUkUC1gU4L0Am_qi_KGYQEHQ3JnANhr682NWY0")
+            @Schema(description = "엑세스 토큰", example = "eyJhbG...")
             String accessToken
     ) {}
 
     @Schema(description = "내 접속 상태 확인 응답")
     public record VisitorMeResponse(
-            @Schema(description = "사용자 고유 UUID", example = "550e8400-e29b-41d4-a716-446655440000")
+            @Schema(description = "사용자 고유 UUID", example = "550e840...")
             String uuid,
 
             @Schema(description = "약관 동의 여부 (false일 경우 약관 팝업 필요)", example = "true")
