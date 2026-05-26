85fef4da83bf3864a54f87ae714e7a751cd2ae51
Uzrt <wakhj47@naver.com>
2026-05-27 06:18:30 +0900

    혼잡도 기준 수정

--- src/main/java/com/waglewagle/server/domain/festivalMap/service/CongestionService.java
+++ src/main/java/com/waglewagle/server/domain/festivalMap/service/CongestionService.java
@@ -44,9 +44,8 @@ public class CongestionService {
 
     // 혼잡도 4단계 (셀당 절대 인원 기준)
     private int calculateLevel(int count) {
-        if (count >= 31) return 3;
-        if (count >= 16) return 2;
-        if (count >= 6)  return 1;
-        return 0;
+        if (count >= 3) return 4;
+        if (count >= 2) return 3;
+        return 2;
     }
 }
